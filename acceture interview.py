8479045477ff3074225bbee45096ea81fd8506c4
appuraj.korkop <appuraj.korkop@gmail.com>
2023-05-05 13:59:48 +0530

    Create acceture interview.py

---
+++ acceture interview.py
@@ -0,0 +1,12 @@
+# sort the list according to severety
+alerts = [{"id":1, "severity":3, "message":"CPU high"}, {"id":2, "severity":1, "message":"App in crashloop"}, {"id":3, "severity":1, "message":"App in pending stage"}]
+
+sort_severety=[]
+def sort_by_severity(alert):
+    for i in alert:
+        print(alerts[1]["severity"])
+        
+        print(i)
+        
+print(sort_by_severity(alerts))
+    # alert.
