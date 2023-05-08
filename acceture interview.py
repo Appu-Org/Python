97fa072a0208d1bb9a0b72e1b876e40c91aeab6c
appuraj.korkop <appuraj.korkop@gmail.com>
2023-05-08 17:16:44 +0530

    Update acceture interview.py

--- acceture interview.py
+++ acceture interview.py
@@ -10,3 +10,19 @@ def sort_by_severity(alert):
         
 print(sort_by_severity(alerts))
     # alert.
+
+    
+import json,operator
+products = '''\
+[{"id":1, "severity":3, "message":"CPU high"}, {"id":2, "severity":1, "message":"App in crashloop"}, {"id":3, "severity":1, "message":"App in pending stage"}]
+'''
+# Array of JSON Objects
+# products = '''\
+# [{"name": "HDD", "brand": "Samsung", "price": 100},
+#             {"name": "Monitor", "brand": "Dell", "price": 120},
+#             {"name": "Mouse", "brand": "Logitech", "price": 10}]
+# '''
+print(type(products))
+data = json.loads(products)
+data.sort(key=operator.itemgetter('severity'))
+print(json.dumps(data, indent=2))
